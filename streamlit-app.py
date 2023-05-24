6d1850aa3cb0d91daa1a2a102acb0cdde4bd9225
Franck Mairot <franck.mairot@gmail.com>
2023-05-24 20:23:09 +0200

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,36 +1,43 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
-st.title("Analyseur de données SEO")
+@st.cache
+def load_data(uploaded_file):
+    data = pd.read_csv(uploaded_file, delimiter = "\t")
+    data['CTR'] = data['CTR'].str.replace('%', '').astype(float) / 100
+    data['Impressions'] = data['Impressions'].str.replace('\u202f', '').astype(int)
+    data['Clicks'] = data['Clicks'].str.replace('\u202f', '').astype(int)
+    data['Lang'] = np.where(data['Page'].str.contains("/fr/"), "FR",
+                      np.where(data['Page'].str.contains("/en/"), "EN", "Other"))
+    return data
 
-uploaded_file = st.file_uploader("Veuillez envoyer votre fichier CSV", type=['csv'])
+uploaded_file = st.file_uploader("Choose a file")
 
 if uploaded_file is not None:
-    data = pd.read_csv(uploaded_file, thousands=' ', decimal=',')
-    
-    # Conversion des colonnes 'Clicks' et 'Impressions' en entiers
-    data['Clicks'] = data['Clicks'].str.replace('\u202f', '').astype(int)
-    data['Impressions'] = data['Impressions'].str.replace('\u202f', '').astype(int)
+    data = load_data(uploaded_file)
 
-    # Conversion de la colonne 'CTR' en décimal, après avoir supprimé le signe '%'
-    data['CTR'] = pd.to_numeric(data['CTR'].str.replace('%', ''), errors='coerce') / 100
+    # Afficher le tableau de données
+    st.write(data)
 
-    # Ajout d'une nouvelle colonne 'Lang' pour identifier la langue de la page
-    data['Lang'] = np.where(data['Page'].str.contains("/fr/"), "FR",
-                        np.where(data['Page'].str.contains("/en/"), "EN", "Autre"))
+    # Créer des graphiques pour les mots-clés étrangers qui rankent avec des pages FR et EN
+    foreign_keywords_fr = data[(data['Lang'] == 'FR') & (data['Query'].str.contains('[a-zA-Z]'))]
+    foreign_keywords_en = data[(data['Lang'] == 'EN') & (data['Query'].str.contains('[a-zA-Z]'))]
 
+    # Création des graphiques
+    fig, ax = plt.subplots(2, 1, figsize=(10, 10))
 
-    # Affichage du nombre de pages par langue
-    st.subheader("Nombre de pages par langue")
-    fig, ax = plt.subplots()
-    data['Lang'].value_counts().plot(kind='bar', ax=ax)
+    ax[0].bar(foreign_keywords_fr['Query'], foreign_keywords_fr['Impressions'])
+    ax[0].set_title('Impressions des mots-clés étrangers sur les pages FR')
+    ax[0].set_xticklabels(foreign_keywords_fr['Query'], rotation=90)
+
+    ax[1].bar(foreign_keywords_en['Query'], foreign_keywords_en['Impressions'])
+    ax[1].set_title('Impressions des mots-clés étrangers sur les pages EN')
+    ax[1].set_xticklabels(foreign_keywords_en['Query'], rotation=90)
+
+    plt.tight_layout()
     st.pyplot(fig)
-
-    # Affichage des mots-clés qui rankent à la fois en français et en anglais
-    st.subheader("Mots-clés qui rankent à la fois en français et en anglais")
-    keywords_fr_en = data[data['Lang'].isin(['FR', 'EN'])]['Query'].value_counts()
-    st.write(keywords_fr_en[keywords_fr_en > 1])
-
+    
 else:
     st.warning("Veuillez uploader un fichier CSV.")
